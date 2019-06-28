3f9460d760dbed108561e32bb4b36fb32ddc7164
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-06-28 16:52:52 +0530

    Create WITHOUT_SYNCHRONIZATION.cs

---
+++ WITHOUT_SYNCHRONIZATION.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using System.Threading;
+/* Synchronization is a technique that allows only one thread to access 
+* the resource for the particular time. No other thread can 
+* interrupt until the assigned thread finishes its task.
+* 
+* ---- Consistency Maintain
+----- No Thread Interference
+
+We can use C# lock keyword to execute program synchronously.
+It is used to get lock for the current thread, execute the task and then release the lock.
+It ensures that other thread does not interrupt the execution until the execution finish.
+Here, we are creating two examples that executes asynchronously and synchronously.
+*/
+
+
+  //* WITHOUT SYNCHRONIZATION *//
+
+namespace ConsoleApp51
+{
+  class Printer
+  {
+    public void PrintTable()
+    {
+      for (int i = 0; i <= 10; i++)
+      {
+        Thread.Sleep(100);
+        Console.WriteLine(i);
+      }
+    }
+  }
+  public class Program
+  {
+    public static void Main()
+    {
+      Printer p = new Printer();
+      Thread t1 = new Thread(new ThreadStart(p.PrintTable));
+      Thread t2 = new Thread(new ThreadStart(p.PrintTable));
+      t1.Start();
+      t2.Start();
+    }
+  }
+}
