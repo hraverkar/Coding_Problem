0b40a6c805a33ab816a430ba3a492aaa660993f5
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-06-28 17:17:55 +0530

    Create FactorialNumber.cs

---
+++ FactorialNumber.cs
@@ -0,0 +1,16 @@
+using System;
+namespace ConsoleApp51
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      int n2 = 1, number = 5; 
+      for(int i=1;i<=number;i++)
+      {
+        n2 = n2 * i;
+      }
+      Console.WriteLine(n2);
+    }
+  }
+}
