8d68c3c05db63b5a049217a48da7aed31c641ba1
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-07-16 22:28:56 +0530

    Update WordOccurrence.cs

--- WordOccurrence.cs
+++ WordOccurrence.cs
@@ -1,42 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp41
+namespace ReturnMaximumOccuringCharacter
 {
   class Program
   {
-    /* static void Main(string[] args)
-      {
-        Console.WriteLine("Hello World!");
-        string str = "HelloWorld";
-        var len = str.Length;
-        while(len > 0)
-        {
-          Console.Write(str[0] + "=");
-          int count  0;
-          for(int i=0;i<len;i++)
-          {
-            if(str[0] == str[i])
-            {
-              count++;
-            }
-          }
-          Console.WriteLine(count);
-          str = str.Replace(str[0].ToString(), string.Empty);
-        }
-      }*/
-
-
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-      string input = "HelloWorld";
-      
-      while (input.Length > 0)
+      Console.WriteLine("Hello World!");
+      string str = "HelloHarshalRaverkar";
+      MaximumOccuringCharater(str);
+    }
+
+    private static void MaximumOccuringCharater(string str)
+    {
+      var input = str.ToLower();
+       while (input.Length > 0)
       {
         int count = 0;
         Console.Write(input[0] + " : ");
-        for(int i=0;i<input.Length;i++)
+        for (int i = 0; i < input.Length; i++)
         {
-          if(input[0] == input[i])
+          if (input[0] == input[i])
           {
             count++;
           }
@@ -48,3 +33,4 @@ namespace ConsoleApp41
     }
   }
 }
+
